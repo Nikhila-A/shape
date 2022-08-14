c3c30e68b743cf535460be74f88fd19bd291f47f
Nikhila-A <akuluru.nikhila@shopup.org>
2022-08-14 12:55:01 +0530

    Create tests for ishape

---
+++ IShape_test.go
@@ -0,0 +1,16 @@
+package shape
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestShapes(t *testing.T) {
+	t.Run("check if the rectangle implements from IShape", func(t *testing.T) {
+		assert.Implements(t, new(IShape), NewRectangle(10.0, 2.0))
+	})
+
+	t.Run("check if the square implements from IShape", func(t *testing.T) {
+		assert.Implements(t, new(IShape), NewSquare(10.0))
+	})
+}
